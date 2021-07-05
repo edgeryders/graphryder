5a43a044ed0221d2e81031bd066748c6e40811b8
Benoit Simard <contact@bsimard.com>
2021-07-05 16:05:40 +0200

    [backend] Increase cache time of Apollo

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -33,7 +33,7 @@ export interface Config {
 export const config: Config = {
   port: process.env.BACKEND_PORT ? Number(process.env.BACKEND_PORT) : 4000,
   error_with_stack: process.env.ERROR_WITH_STACK ? true : false,
-  graphql_cache_max_age: 3600,
+  graphql_cache_max_age: 86400,
   logs: {
     console_level: (process.env.LOG_CONSOLE_LEVEL || "info") as LogLevel,
     file_level: (process.env.LOG_FILE_LEVEL || "error") as LogLevel,
